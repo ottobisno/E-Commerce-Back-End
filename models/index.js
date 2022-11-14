6aace288ea93edfd8ec9f909575ef5464921992f
OttoBisno <ottobisno444@gmail.com>
2022-11-13 18:50:37 -0800

    Updated /models/index.js with relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    mode: ProductTag,
+    unique: false
+  },
+  as: 'product_to_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    mode: ProductTag,
+    unique: false
+  },
+  as: 'tags_to_products'
+});
 
 module.exports = {
   Product,
